4294ce67d5a99b2e62ce96dc295dc67dace6b1c2
C-Skala <cjskala@gmail.com>
2022-06-20 21:25:29 -0400

    welcome and rules created

---
+++ gameplay.py
@@ -0,0 +1,22 @@
+
+
+def welcome_and_rules(self):
+    print('welcome to the hardened world where the fastest hand wins')
+    print('the rules are simple, pick a number corelated to one of our weapons and hope it is stronger than your oponent')
+    print('the weapons and how they compaire are as follows:')
+    print("")
+    print("")
+    print("")
+    print("Rock crushes Scissors")
+    print("Scissors cuts Paper")
+    print("Paper covers Rock")
+    print("Rock crushes Lizard")
+    print("Lizard Poisons Spock")
+    print("Spock smashes Scissors")
+    print("Scissors decapitates Lizard")
+    print("Lizard eats Paper")
+    print("Paper disproves Spock")
+    print("Spock vaporizes Rock")
+    print("")
+    print("")
+    print("to ensure this war doesnt wage on for eternity a winner will be declaired after three superior weapon picks")
