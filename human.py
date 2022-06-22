c9b555ec3a9bcc3988bce7abca77f37ce516eba6
C-Skala <cjskala@gmail.com>
2022-06-21 21:23:18 -0400

    added better functions to pick_a_weapon to flow better

--- human.py
+++ human.py
@@ -7,16 +7,21 @@ class Human(Player):
         self.set_name()
 
     def pick_a_weapon(self):
-        self.chosen_gesture = randint(0,4)
-        if self.chosen_gesture == 0:
+        self.chosen_gesture = input('please select a number. \n1-Rock\n2-Paper\n3-Scissors\n4-Lizzard\n5-Spock')
+        if self.chosen_gesture == 1:
+            self.chosen_gesture == 'Rock'
             print('player picked Rock')
-        elif self.chosen_gesture == 1:
-            print('player picked Paper')
         elif self.chosen_gesture == 2:
-            print('player picked Scissors')
+            self.chosen_gesture == 'Paper'
+            print('player picked Paper')
         elif self.chosen_gesture == 3:
-            print('player picked Lizzard')
+            self.chosen_gesture == 'Scissors'
+            print('player picked Scissors')
         elif self.chosen_gesture == 4:
+            self.chosen_gesture == 'Lizzard'
+            print('player picked Lizzard')
+        elif self.chosen_gesture == 5:
+            self.chosen_gesture == 'Spock'
             print('player picked Spock')
 
     def set_name(self):
--- player.py
+++ player.py
@@ -11,12 +11,17 @@ class Player:
     def pick_a_weapon(self):
         self.chosen_gesture = randint(0,4)
         if self.chosen_gesture == 0:
+            self.chosen_gesture == 'Rock'
             print('player picked Rock')
         elif self.chosen_gesture == 1:
+            self.chosen_gesture == 'Paper'
             print('player picked Paper')
         elif self.chosen_gesture == 2:
+            self.chosen_gesture == 'Scissors'
             print('player picked Scissors')
         elif self.chosen_gesture == 3:
+            self.chosen_gesture == 'Lizzard'
             print('player picked Lizzard')
         elif self.chosen_gesture == 4:
+            self.chosen_gesture == 'Spock'
             print('player picked Spock')
